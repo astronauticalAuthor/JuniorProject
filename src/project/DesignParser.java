79cb06d8adab7a18b5d3a2dc858a81cd234305d2
Katrina <katrina.kerrick@gmail.com>
2016-01-07 18:32:50 -0500

    It works now! Just need to test and hook up graphviz. 30 minutes of Katrina programming

--- src/project/DesignParser.java
+++ src/project/DesignParser.java
@@ -11,7 +11,6 @@ import java.io.*;
 
 public class DesignParser {
 	public static void main(String[] args) throws IOException{
-		ArrayList<ClassField> classes = new ArrayList<ClassField>();
 		int index = 0;
 		for(String className: args){
 			ClassField current = new ClassField();
@@ -23,15 +22,21 @@ public class DesignParser {
 			ClassFieldVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, current);
 			ClassMethodVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, current);
 			
-			reader.accept(declVisitor, ClassReader.EXPAND_FRAMES);			
+			reader.accept(declVisitor, ClassReader.EXPAND_FRAMES);
+			reader.accept(fieldVisitor, ClassReader.EXPAND_FRAMES);
+			reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
 			
-			classes.add(current);
-//			ClassRepresentation.addClass(declVisitor.getClassInfo());
-//			ClassRepresentation.addField(index, fieldVisitor.getFieldInfo());
-//			ClassRepresentation.addMethod(index, methodVisitor.getMethodInfo());
+			ClassRepresentation.classes.add(current);
 			index++;
 		}
 		
-		System.out.println("\n\n\n");
+		System.out.println("\n\n\n" + ClassRepresentation.convert());
+	
+		try {
+			PrintWriter writer = new PrintWriter(new File("output.dot"));
+			writer.write(ClassRepresentation.convert());
+			writer.close();
+		}
+		catch(Exception e) {}
 	}
 }
